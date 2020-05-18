493d1ed7e85145e952951b2661653e95ab9943e9
mattmannec <mattmannec@gmail.com>
2020-05-18 15:32:42 +0200

    changed inherit window sized

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings:
     def __init__(self):
     
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 0
+        self.screen_height = 0
         self.bg_color = (0, 0, 0)
 
         # Paddle settings
